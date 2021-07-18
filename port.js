8ea102488bd5839f14ca241849b1604e1ce5addf
Md Atif Ansar <atifansar002@gmail.com>
2021-07-18 13:36:10 +0530

    Commit 1

---
+++ port.js
@@ -0,0 +1,17 @@
+
+$(function () {
+  $('[data-toggle="tooltip"]').tooltip()
+});
+
+$(document).ready(function(){
+
+    setTimeout(() => {
+      $(".cover h1").slideDown();
+    }, 1200);
+
+    setTimeout(() => {
+      $(".cover p").slideDown();
+    }, 1000);
+
+  });
+
