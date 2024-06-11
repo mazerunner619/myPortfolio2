9f2279b8393a7e32d92d9779ce4e7ff80703023b
Ansar <md.atif.ansar@accenture.com>
2024-06-11 13:08:45 +0530

    updated experience, skills

--- port.js
+++ port.js
@@ -1,44 +1,38 @@
-
 $(function () {
-  $('[data-toggle="tooltip"]').tooltip()
+  $('[data-toggle="tooltip"]').tooltip();
 });
 
-
-window.onscroll = function() {
-  if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
-    $(".nav-item").css('color', 'rgba(0, 0, 0, 0.7)');
-    $(".navbar-brand").css('color', 'black');
-    document.getElementById("navbar").style.background = "rgba(0,255,255,0.837)";
+window.onscroll = function () {
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    $(".nav-item").css("color", "rgba(0, 0, 0, 0.7)");
+    $(".navbar-brand").css("color", "black");
+    document.getElementById("navbar").style.background =
+      "rgba(0,255,255,0.837)";
   } else {
-    $(".nav-item").css('color', 'rgba(255,255,255,0.6)');
-    $(".navbar-brand").css('color', 'white');
+    $(".nav-item").css("color", "rgba(255,255,255,0.6)");
+    $(".navbar-brand").css("color", "white");
     document.getElementById("navbar").style.background = "none";
   }
-}
+};
 
-var i=0;
+var i = 0;
 
-$("#tbtn").click(function(){
-  if(i == 0){
-    $(".navbar").css('margin-bottom', '-1%');
-    i=1;
+$("#tbtn").click(function () {
+  if (i == 0) {
+    $(".navbar").css("margin-bottom", "-1%");
+    i = 1;
+  } else {
+    $(".navbar").css("margin-bottom", "-12%");
+    i = 0;
   }
-  else{
-    $(".navbar").css('margin-bottom', '-12%');
-    i=0;
-  }
-  
 });
 
-$(document).ready(function(){
+$(document).ready(function () {
+  setTimeout(() => {
+    $(".cover h1").slideDown();
+  }, 1200);
 
-    setTimeout(() => {
-      $(".cover h1").slideDown();
-    }, 1200);
-
-    setTimeout(() => {
-      $(".cover p").slideDown();
-    }, 1000);
-
-  });
-
+  setTimeout(() => {
+    $(".cover p").slideDown();
+  }, 1000);
+});
